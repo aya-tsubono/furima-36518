8b217eaf5dc5363cfc08106ae6cb70473e4be92b
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-10-20 13:12:26 +0900

    バリデーションの訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :encrypted_password,length:{minimum: 6 },format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
-  validates :email, uniqueness: true, format: {with:  /\A\S+@\S+\.\S+\z/ }
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :encrypted_password,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
 
 end
