82959dbb70fd8bc2b8a9b93948b42c13a4acd011
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-10-20 12:41:03 +0900

    ユーザー情報のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :email, unique: true
+  validates :encrypted_password,length:{minimum: 6 },format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+  validates :email, uniqueness: true, format: {with:  /\A\S+@\S+\.\S+\z/ }
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
