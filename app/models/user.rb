4a9901405877681ce8e805bdd4e79bbcaecc259e
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-10-22 17:25:12 +0900

    rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :nickname, presence: true
-  validates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :birthday, presence: true
 
+  validates :nickname, presence: true
+  validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.name}
-    email {Faker::Internet.free_email}
-    password {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
+    nickname { Faker::Name.name }
+    email { Faker::Internet.free_email }
+    password { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation { password }
     last_name { Gimei.last.kanji }
     first_name { Gimei.first.kanji }
