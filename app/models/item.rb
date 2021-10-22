2f4583ba7c2ac2fcbe131f335e3d2eb6d983da3d
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-10-23 00:26:40 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+
+  validates :title, presence: true
+  validates :text, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :area_id, presence: true
+  validates :days, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
 end
