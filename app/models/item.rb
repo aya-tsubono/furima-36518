92ab0a771e8dea4d19447d588872fbfc322c4cf0
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-10-23 17:29:55 +0900

    delivery_feeの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   validates :text, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" } 
   validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :delivery_fee_id, presence: true
+  validates :delivery_fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank" } 
   validates :area_id, presence: true
   validates :days, presence: true
   validates :price, presence: true
@@ -14,8 +14,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition
-
+  belongs_to :category, :condition, :delivery_fee
 
 
 end
