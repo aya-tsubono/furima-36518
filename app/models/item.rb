59248003ac540c5082e61c497caf3316d3dee03a
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-10-23 17:27:02 +0900

    conditionの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   validates :title, presence: true
   validates :text, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :condition_id, presence: true
+  validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" } 
   validates :delivery_fee_id, presence: true
   validates :area_id, presence: true
   validates :days, presence: true
@@ -14,8 +14,8 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
+  belongs_to :category, :condition
 
-  
+
 
 end
