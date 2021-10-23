a74189d17a8b61fec5ea577394baebb6bf1361ac
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-10-23 16:46:25 +0900

    categoryのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
 
   validates :title, presence: true
   validates :text, presence: true
-  validates :category_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" } 
   validates :condition_id, presence: true
   validates :delivery_fee_id, presence: true
   validates :area_id, presence: true
