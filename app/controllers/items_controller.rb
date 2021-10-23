f9b5f4100384f0d5f011a630d84c2618e6a5093b
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-10-23 16:34:30 +0900

    createコントローラーの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,23 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :text, :category_id, :condition_id, :delivery_fee_id, :area_id, :days, :price,:image).merge(user_id: current_user.id)
   end
 
 end
