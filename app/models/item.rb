f0ae154e63271c42d66ac25b1ffd573b06aa4c1a
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-10-23 17:37:31 +0900

    daysの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,8 @@ class Item < ApplicationRecord
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" } 
   validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" } 
   validates :delivery_fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :area_id, presence: true
-  validates :days, presence: true
+  validates :area_id, presence: true, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :days_id, presence: true, numericality: { other_than: 1, message: "can't be blank" } 
   validates :price, presence: true
   validates :user, presence: true
 
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :delivery_fee
+  belongs_to :category, :condition, :delivery_fee, :area, :days
 
 
 end
