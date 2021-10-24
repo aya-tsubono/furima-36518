8701b3f199c5a6267ab24e191e0af8440ce849f9
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-10-24 16:28:10 +0900

    投稿ビューの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  
+
   def index
     @items = Item.order("created_at DESC")
   end
 
   def new
     @item = Item.new
+    @user = User.new
   end
 
   def create
