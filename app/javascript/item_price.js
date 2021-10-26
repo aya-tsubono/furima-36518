ad3aa39c7637c4c602bb916c62140d5ecb297183
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-10-27 01:56:44 +0900

    販売価格のJavaScriptの記述

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,13 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const Tax = 0.1
+    const price = inputValue * Tax
+    addTaxDom.innerHTML = Math.floor(price)
+    const gain = inputValue - price
+    profit.innerHTML = Math.floor(gain)
+  })
 });
