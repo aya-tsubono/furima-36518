d4e63990e42fa1a94d980bce0c926e9ccd06db94
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-07 18:34:17 +0900

    マイグレーションファイルの修正

--- db/migrate/20211107090419_create_buying_histories.rb
+++ db/migrate/20211107090419_create_buying_histories.rb
@@ -1,8 +1,8 @@
 class CreateBuyingHistories < ActiveRecord::Migration[6.0]
   def change
     create_table :buying_histories do |t|
-      t.references :price, null: false, foreign_key: true
-      t.references :user,  null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
