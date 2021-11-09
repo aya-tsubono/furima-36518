44051817a5095d404884b02796f1a35197eefeb0
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-09 16:43:06 +0900

    スペルの修正

--- app/models/buying_history_address.rb
+++ app/models/buying_history_address.rb
@@ -1,6 +1,6 @@
-class BuyingHistoryAddress < ApplicationRecord
+class BuyingHistoryAddress
   include ActiveModel::Model
-  attr_accessor :postcode, :prefectures, :municipality, :addresses, :building, :phone_number, :buying_history_id, :token
+  attr_accessor :postcode, :prefecture, :municipality, :addresses, :building, :phone_number, :buying_history_id, :token
   
     validates :postcode,          presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :municipality,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
@@ -9,12 +9,12 @@ class BuyingHistoryAddress < ApplicationRecord
     validates :buying_history_id, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefectures
-  validates :prefectures_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
+  belongs_to :prefecture
+  validates :prefecture_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
     buying_history = BuyingHistory.create(item_id: item_id, user_id: user_id)
-    Address.create(postcode: postcode, prefectures: prefectures, municipality: municipality, addresses: addresses, building: building, phone_number: phone_number, buying_history_id: buying_history.id)
+    Address.create(postcode: postcode, prefecture: prefecture, municipality: municipality, addresses: addresses, building: building, phone_number: phone_number, buying_history_id: buying_history.id)
   end
 
 end
--- db/migrate/20211107090453_create_addresses.rb
+++ db/migrate/20211107090453_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.string  :postcode,          null: false
-      t.integer :prefectures_id,    null: false
+      t.integer :prefecture_id,     null: false
       t.string  :municipality,      null: false
       t.string  :addresses,         null: false
       t.string  :building
