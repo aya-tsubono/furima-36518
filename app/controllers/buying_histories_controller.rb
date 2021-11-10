88c02b30c56d79b88fae129c8f871c4a46c86343
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-10 23:25:15 +0900

    販売済商品の購入ページへ遷移できないように記述

--- app/controllers/buying_histories_controller.rb
+++ app/controllers/buying_histories_controller.rb
@@ -1,25 +1,32 @@
 class BuyingHistoriesController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    elsif (current_user.id != @item.user_id) && @item.buying_history.present?
+      redirect_to root_path
+    end
     @buying_history_address = BuyingHistoryAddress.new
   end
 
-
   def create
-    @item = Item.find(params[:item_id])
     @buying_history_address = BuyingHistoryAddress.new(buying_history_params)
     if @buying_history_address.valid?
       pay_item
       @buying_history_address.save
-      return redirect_to root_path
+      redirect_to root_path
     else
       render 'index'
     end
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def buying_history_params
     params.require(:buying_history_address).permit(:postcode, :area_id, :municipality, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
