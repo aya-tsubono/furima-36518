bfa670138dfd5a43935718bca3b00474bc687825
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2021-11-10 17:30:32 +0900

    fakerの記述

--- spec/factories/buying_history_addresses.rb
+++ spec/factories/buying_history_addresses.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :buying_history_address do
-    
+    postcode { '123-4567' }
+    area_id { Faker::Number.between(from: 2, to: 48) }
+    municipality { Gimei.address.city.kanji }
+    addresses { Gimei.address.town.kanji }
+    building { '田中ビル１０１' }
+    phone_number { Faker::PhoneNumber.cell_phone }
+    association :buying_history
+    association :user
+    association :Item
   end
 end
